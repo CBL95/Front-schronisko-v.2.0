f101f536a07a88c73f775fe800248bf0b6e8fd29
Grzegorz Brzymowski <g.brzymowski@gmail.com>
2023-01-22 23:54:25 +0100

    usunięcie zbędnych komentarzy i "upiększenie" kodu

--- src/components/Animal.vue
+++ src/components/Animal.vue
@@ -13,54 +13,54 @@
                 <th>Usuwanie</th>
             </thead>
             <tbody>
-                <tr v-for = "animal in animals" v-bind:key="animal.id">
-                    <td> {{animal.id}}  </td>
-                    <td> {{animal.name}}  </td>
-                    <td> {{animal.category}}</td>
-                    <td> {{animal.sex}} </td>
-                    <td> {{animal.age}} </td>
-                    <td> {{animal.color}}  </td>
-                    <td> {{animal.size}} </td>
-                    <td><button v-on:click="deleteAnimal(animal.id)" >Usuń</button></td>
+                <tr v-for="animal in animals" v-bind:key="animal.id">
+                    <td> {{ animal.id }} </td>
+                    <td> {{ animal.name }} </td>
+                    <td> {{ animal.category }}</td>
+                    <td> {{ animal.sex }} </td>
+                    <td> {{ animal.age }} </td>
+                    <td> {{ animal.color }} </td>
+                    <td> {{ animal.size }} </td>
+                    <td><button v-on:click="deleteAnimal(animal.id)">Usuń</button></td>
                 </tr>
             </tbody>
         </table>
-      
+
     </div>
-    
+
 </template>
 
 <script>
-/* eslint-disable */ 
+/* eslint-disable */
 /*jshint esversion: 6 */
 import AnimalService from '@/services/AnimalService';
 
 
 export default {
-        name:'Animals',
-        data(){
-            return{
-                animals : []
-            };
+    name: 'Animals',
+    data() {
+        return {
+            animals: []
+        };
 
+    },
+    methods: {
+        getAnimals() {
+            AnimalService.getAnimals().then((response) => {
+                this.animals = response.data;
+            })
         },
-        methods: {
-            getAnimals(){
-                AnimalService.getAnimals().then((response) =>{
-                    this.animals=response.data;
-                })
-            },
-            deleteAnimal(id) {
-                AnimalService.deleteAnimal(id).then(()=>{
-                    this.getAnimals();
-                })
-            },
-            /*dodalem komentarz do testu gita */
-            
+        deleteAnimal(id) {
+            AnimalService.deleteAnimal(id).then(() => {
+                this.getAnimals();
+            })
         },
-        created(){
-            this.getAnimals();
-        }
-    };
+
+
+    },
+    created() {
+        this.getAnimals();
+    }
+};
 </script>
 
